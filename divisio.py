390d0afbad0239686aaeaca22c982b043f378049
albertangelet <aangelet@instituticaria.cat>
2025-10-15 11:02:28 +0200

    divisió

--- divisio.py
+++ divisio.py
@@ -3,11 +3,11 @@ divisor = int(input("Introdueix el divisor: "))
 if divisor == 0:
     print("Error: no es pot dividir entre zero.")
 else:
-    divisio = dividend / divisor
-    quocient = dividend // divisor
+    divisio = dividend/divisor
+    quocient = dividend//divisor
     residu = dividend % divisor
 
     # Mostrar els resultats
-    print(f"Divisió: {dividend} / {divisor} = {divisio}")
+    print(f"Divisió: {divisio}")
     print(f"Quocient: {quocient}")
     print(f"Residu: {residu}")
